1240cbfb0fd56021116c987541669b1c96713b27
robertows <145810108+robertows@users.noreply.github.com>
2025-04-12 09:06:36 -0300

    Update script.js
    git add . + git commit -m "Trocando bcrypt por bcryptjs" + git push origin

--- docs/script.js
+++ docs/script.js
@@ -1136,7 +1136,7 @@ function inicializarEventos() {
             }
 
             try {
-                const response = await fetch(`${API_URL}/login`, {
+                const response = await fetch(`${API_URL}/auth/login`, {
                     method: 'POST',
                     headers: { 'Content-Type': 'application/json' },
                     body: JSON.stringify({ usuario, senha }),
