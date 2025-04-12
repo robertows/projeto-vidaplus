2e256f9b9e1a14dd66cf78420eabcd779d989ed4
robertows <145810108+robertows@users.noreply.github.com>
2025-04-11 23:04:10 -0300

    Update script.js
    git add .
    git commit -m "Trocando url Railway"
    git push origin main

--- docs/script.js
+++ docs/script.js
@@ -20,7 +20,6 @@ function decodeJWT(token) {
 
 // URL base da API
 const API_URL = 'https://projeto-vidaplus-production.up.railway.app';
-
 function getUsuarioLogado() {
     const usuario = localStorage.getItem('usuarioLogado');
     return usuario ? JSON.parse(usuario) : null;
